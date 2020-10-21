c3a3b3296a70715994be4878a942876ef0c21b47
elmetal <nm7.pupu@gmail.com>
2020-10-21 15:23:27 +0900

    ttt game

--- Sources/SwiftWasmPlaygrounds/ContentView.swift
+++ Sources/SwiftWasmPlaygrounds/ContentView.swift
@@ -1,17 +1,10 @@
 import TokamakShim
 
 struct ContentView: View {
-    @State var text = "0"
     var body: some View {
-        NavigationView {    
-            VStack {
-                Text(text)
-                HStack(alignment: .bottom) {
-                    NumberPad(text: $text)
-                    CalcPad()
-                }
-                NavigationLink(destination: Text("hello")) { Text("navigation link") }
-            }
+        VStack {
+            HeaderView()
+            TTTGameBoard()
         }
     }
 }
---
+++ Sources/SwiftWasmPlaygrounds/Header.swift
@@ -0,0 +1,9 @@
+import TokamakShim
+
+struct HeaderView: View {
+    var body: some View {
+        Text("Hello SwiftWasmPlaygrounds")
+          .font(.title)
+    }
+}
+
